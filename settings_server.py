70475394fcd6ea19eb14eb032f60681d46d2a685
olivaC <coliva@ualberta.ca>
2019-02-19 14:15:10 -0700

    Update gitignore and add settings_server to pass builds

---
+++ settings_server.py
@@ -0,0 +1,29 @@
+import os
+
+# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+
+# Quick-start development settings - unsuitable for production
+# See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = '^jh^#o5qwav+=k#^6wmsd4k#sa)31)um!^#e5-e38$u57cm@r7'
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+ALLOWED_HOSTS = []
+
+# Database
+# https://docs.djangoproject.com/en/2.1/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
+
+# This will have to change when added to production server
+DOMAIN = "http://127.0.0.1:8000"
