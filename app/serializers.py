549d418b1aa9180025914cfc64ec5c305c93a4ac
carrolji <jirakul@ualberta.ca>
2019-02-25 23:19:04 -0700

    #3 modified author serializer

--- app/serializers.py
+++ app/serializers.py
@@ -1,8 +1,17 @@
 from rest_framework import serializers
+from app.models import *
 
 
 class AuthorSerializers(serializers.ModelSerializer):
-    username = serializers.CharField(source='user.username')
-    description = serializers.CharField(source='user.description')
-    host_url = serializers.URLField(source='user.host_url')
-    github_url = serializers.CharField(source='user.github_url')
+
+    class Meta:
+        model = Author
+        fields = (
+            'id',
+            'username',
+            'description',
+            'host_url',
+            'github_url',
+            'user',
+        )
+        depth = 2
