a65f247a97eca92dc805e8a6c7adb1dd9ac65b65
olivaC <coliva@ualberta.ca>
2019-03-20 15:04:33 -0600

    #41 Add friend request and unfollow api endpoint

--- app/serializers.py
+++ app/serializers.py
@@ -28,3 +28,24 @@ class PostSerializer(serializers.ModelSerializer):
         model = Post
         fields = ('id', 'published', 'author', 'title', 'content', 'contentType', 'visibility', 'unlisted')
         depth = 1
+
+
+class FollowRequestSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = FollowRequest
+        fields = ('id',
+                  'author',
+                  'friend')
+
+    def create(self, validated_data):
+        if validated_data.get('author'):
+            author = validated_data.get('author')
+            friend = validated_data.get('friend')
+            FollowRequest.objects.create(
+                author=author,
+                friend=friend
+            )
+            auth_follow = FollowRequest.objects.all().filter(friend=author).filter(author=friend)
+            if auth_follow:
+                author.friends.add(friend)
+                author.save()
--- app/views/api_views.py
+++ app/views/api_views.py
@@ -1,4 +1,8 @@
 from rest_framework import viewsets
+from rest_framework.generics import DestroyAPIView
+from rest_framework.response import Response
+from rest_framework import permissions
+
 from app.serializers import *
 
 
@@ -34,3 +38,36 @@ class PostView(viewsets.ModelViewSet):
             posts = Post.objects.all().filter(author=author) | Post.objects.all().filter(visibility="PUBLIC")
             posts = posts.order_by('-published')
             return posts
+
+
+class AuthorPostView(viewsets.ModelViewSet):
+    """
+    The api view to retrieve posts.
+    """
+    serializer_class = PostSerializer
+    allowed_methods = ['GET', 'POST', 'PATCH']
+    permission_classes = (permissions.IsAuthenticated,)
+
+    def get_queryset(self):
+        author = self.request.user.user
+        posts = Post.objects.all().filter(author=author).order_by('-published')
+        return posts
+
+
+class FollowRequestView(viewsets.ModelViewSet):
+    """
+    The api view to create a follow request
+    """
+    serializer_class = FollowRequestSerializer
+    allowed_methods = ['GET', 'POST', 'DELETE']
+    permission_classes = (permissions.IsAuthenticated,)
+
+    def get_queryset(self):
+        if self.request.user.is_staff:
+            queryset = FollowRequest.objects.all().order_by("-date_created")
+            return queryset
+        else:
+            queryset = FollowRequest.objects.all().filter(author=self.request.user.user).order_by('-date_created')
+            return queryset
+
+
