eb0d33c76dc8dbfb1ad99c5c9c983eb9c991f104
Alex Li <tianyi7@ualberta.ca>
2019-03-20 16:29:27 -0600

    add basic authentication to rest api

--- SocialDistribution/settings.py
+++ SocialDistribution/settings.py
@@ -40,6 +40,7 @@ MIDDLEWARE = [
 # Django rest framework
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.BasicAuthentication',
         'rest_framework.authentication.SessionAuthentication'
     ]
 }
