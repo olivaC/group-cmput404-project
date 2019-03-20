8000d62cc2e83df7f736d7c6e5383311a9cf9657
olivaC <coliva@ualberta.ca>
2019-03-20 17:22:41 -0600

    Add comments in mutual friend view

--- app/views/api_views.py
+++ app/views/api_views.py
@@ -1,6 +1,4 @@
 from rest_framework import viewsets
-from rest_framework.generics import DestroyAPIView
-from rest_framework.response import Response
 from rest_framework import permissions
 
 from app.serializers import *
@@ -69,5 +67,3 @@ class FollowRequestView(viewsets.ModelViewSet):
         else:
             queryset = FollowRequest.objects.all().filter(author=self.request.user.user).order_by('-date_created')
             return queryset
-
-
