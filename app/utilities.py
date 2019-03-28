ccff9b04da3dd533036189aeac9f04db8f354fbe
olivaC <coliva@ualberta.ca>
2019-03-28 17:04:18 -0600

    Fix error

--- app/utilities.py
+++ app/utilities.py
@@ -20,7 +20,7 @@ def unquote_redirect_url(url):
 
 
 def api_check(user):
-    if ('frandzone', 'testApi') in user.username:
+    if 'frandzone' in user.username or 'testApi' in user.username:
         return False
     else:
         return True
