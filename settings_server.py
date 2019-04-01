08adfce00e0b2f79dcf93120a7bc3584a3b25391
olivaC <coliva@ualberta.ca>
2019-04-01 13:47:16 -0600

    Diff settings

--- settings_server.py
+++ settings_server.py
@@ -26,7 +26,7 @@ DATABASES = {
 
 # This will have to change when added to production server
 if 'I_AM_HEROKU' in os.environ:
-    DOMAIN = "https://cmput404group10.herokuapp.com"
+    DOMAIN = "https://young-plains-33934.herokuapp.com/api"
 else:
     DOMAIN = "http://127.0.0.1:8000"
 
