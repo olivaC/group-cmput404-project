2066f0152fd89fb62bcceca4f585037eb5091429
olivaC <coliva@ualberta.ca>
2019-04-01 15:43:59 -0600

    Update

--- api/api_utilities.py
+++ api/api_utilities.py
@@ -249,7 +249,7 @@ def get_public_posts(server_posts):
             host = host + "/"
         server_api = "{}posts".format(host)
         try:
-            r = requests.get(server_api, auth=(server.username, server.password))
+            r = requests.get(server_api)
 
             if r.status_code == 200:
                 posts = remotePostList(server.hostname, r.json())
