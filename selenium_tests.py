606a5c27d4baaa8348b0589294b3786b1b8f8155
olivaC <coliva@ualberta.ca>
2019-04-07 20:25:19 -0600

    Add tests

---
+++ selenium_tests.py
@@ -0,0 +1,17 @@
+from selenium import webdriver
+
+
+def test_home():
+    driver = webdriver.Chrome()
+    driver.get('https://cmput404group10.herokuapp.com')
+
+    username = driver.find_element_by_id("username")
+    username.send_keys("ronWeasley")
+
+    password = driver.find_element_by_name("password")
+    password.send_keys("ualberta123")
+
+    
+
+
+
