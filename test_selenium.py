9a771d606416fff8f6bb249f1e8a3a2b0ff335b3
olivaC <coliva@ualberta.ca>
2019-04-07 21:26:25 -0600

    Add edit profile test

--- test_selenium.py
+++ test_selenium.py
@@ -32,5 +32,48 @@ def test_unsuccessful_login():
     exists = driver.find_element_by_xpath('/html/body/div/div/div/div/div/div/div[2]/div/form/div[4]/ul/li')
     assert "Sorry, the username and password could not be found." == exists.text
 
-    
+
+def test_edit_profile():
+    driver = webdriver.Chrome()
+    driver.get('https://cmput404group10.herokuapp.com')
+
+    username = driver.find_element_by_id("username")
+    username.send_keys("ronWeasley")
+
+    password = driver.find_element_by_name("password")
+    password.send_keys("ualberta123")
+
+    driver.find_element_by_xpath('/html/body/div/div/div/div/div/div/div[2]/div/form/div[3]/button').click()
+
+    driver.find_element_by_xpath('//*[@id="userDropdown"]').click()
+    driver.find_element_by_xpath('//*[@id="content"]/nav/ul/li[3]/div/a[1]').click()
+
+    displayName = driver.find_element_by_xpath('//*[@id="content"]/div/div/div').text
+    assert "ronWeasley" == displayName
+
+    driver.find_element_by_xpath('//*[@id="userDropdown"]').click()
+    driver.find_element_by_xpath('//*[@id="content"]/nav/ul/li[3]/div/a[2]').click()
+
+    username = driver.find_element_by_id("id_username")
+    username.clear()
+    username.send_keys("hermioneGranger")
+
+    driver.find_element_by_xpath('//*[@id="content"]/div/form/button').click()
+
+    displayName = driver.find_element_by_xpath('//*[@id="content"]/div/div/div').text
+    assert "hermioneGranger" == displayName
+
+    driver.find_element_by_xpath('//*[@id="userDropdown"]').click()
+    driver.find_element_by_xpath('//*[@id="content"]/nav/ul/li[3]/div/a[2]').click()
+
+    username = driver.find_element_by_id("id_username")
+    username.clear()
+    username.send_keys("ronWeasley")
+
+    driver.find_element_by_xpath('//*[@id="content"]/div/form/button').click()
+
+    displayName = driver.find_element_by_xpath('//*[@id="content"]/div/div/div').text
+    assert "ronWeasley" == displayName
+
+
 
